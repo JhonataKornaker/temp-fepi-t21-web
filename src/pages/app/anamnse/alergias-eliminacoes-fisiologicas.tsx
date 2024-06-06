00dcbbbe7546c69c8ebf4d1f996ba5101131fbf7
Breno Flaubert de Souza <brenoflaubert@gmail.com>
2024-06-06 13:56:49 -0300

    :sparkles: feat: (anamnse) tab alergias e eliminacoes fisiologicas

---
+++ src/pages/app/anamnse/alergias-eliminacoes-fisiologicas.tsx
@@ -0,0 +1,151 @@
+/* eslint-disable prettier/prettier */
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { useForm } from "react-hook-form";
+
+export function AlergiasEliminacoesFisiologicas() {
+  const { register, handleSubmit } = useForm();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const onSubmit = (data: any) => console.log(data);
+  return (
+    <section>
+      <h1 className="text-2xl mb-8">Alergias e Eliminações fisiologicas</h1>
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="mt-4 flex flex-col gap-5"
+      >
+        <section className="flex flex-col w-full">
+          <label className="mb-2">O Atleta posui algum tipo de alergia?</label>
+          <div className="flex items-center gap-8 mb-3">
+            <label className="flex items-center">
+              <input type="radio" value={"yes"} {...register("hasAllergy")} />
+              <span className="ml-2">Sim</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"no"}
+                {...register("hasAllergy")}
+                className="bg-customText"
+              />
+              <span className="ml-2">Não</span>
+            </label>
+          </div>
+          <textarea
+            id="allergyDetails"
+            placeholder="Observação, caso necessário..."
+            {...register("allergyDetails")}
+            className="rounded-md h-24 bg-slate-900 p-1 leading-tight"
+          />
+        </section>
+        <section className="flex flex-col w-full">
+          <label className="mb-2">
+            O Atleta consegue ir ao banheiro sozinho?
+          </label>
+          <div className="flex items-center gap-8 mb-3">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"yes"}
+                {...register("canBathroomAlone")}
+              />
+              <span className="ml-2">Sim</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"no"}
+                {...register("canBathroomAlone")}
+                className="bg-customText"
+              />
+              <span className="ml-2">Não</span>
+            </label>
+          </div>
+          <textarea
+            id="canBathroomAloneDetails"
+            placeholder="Observação, caso necessário..."
+            {...register("canBathroomAloneDetails")}
+            className="rounded-md h-24 bg-slate-900 p-1 leading-tight"
+          />
+        </section>
+        <section className="flex flex-col w-full">
+          <label className="mb-2">
+            O atleta possui incontinência urinária ou fecal?
+          </label>
+          <div className="flex items-center gap-8 mb-3">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"yes"}
+                {...register("incontinence")}
+              />
+              <span className="ml-2">Sim</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"no"}
+                {...register("incontinence")}
+                className="bg-customText"
+              />
+              <span className="ml-2">Não</span>
+            </label>
+          </div>
+          <textarea
+            id="IncontinenceDetails"
+            placeholder="Observação, caso necessário..."
+            {...register("IncontinenceDetails")}
+            className="rounded-md h-24 bg-slate-900 p-1 leading-tight"
+          />
+        </section>
+        <section className="flex flex-col w-full">
+          <label className="mb-2">
+            O atleta faz uso de fraldas?
+          </label>
+          <div className="flex items-center gap-8 mb-3">
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"yes"}
+                {...register("useDiapers")}
+              />
+              <span className="ml-2">Sim</span>
+            </label>
+            <label className="flex items-center">
+              <input
+                type="radio"
+                value={"no"}
+                {...register("useDiapers")}
+                className="bg-customText"
+              />
+              <span className="ml-2">Não</span>
+            </label>
+          </div>
+          <textarea
+            id="useDiapersDetails"
+            placeholder="Observação, caso necessário..."
+            {...register("useDiapersDetails")}
+            className="rounded-md h-24 bg-slate-900 p-1 leading-tight"
+          />
+        </section>
+        <section className="flex flex-col w-full">
+          <label className="mb-2" htmlFor="athelteRoutineDetails">
+            Descreva como é a rotina de necessidades fisiológicas do atleta
+            (quantas vezes vai ao banheiro, se precisa de auxílio... )
+          </label>
+          <textarea
+            id="athelteRoutineDetails"
+            placeholder="Observação, caso necessário..."
+            {...register("athelteRoutineDetails")}
+            className="rounded-md h-24 bg-slate-900 p-1 leading-tight"
+          />
+        </section>
+        <button
+          type="submit"
+          className="w-20 h-10 bg-customText text-customBackground  rounded-md"
+        >
+          <span className="font-semibold">Salvar</span>
+        </button>
+      </form>
+    </section>
+  );
+}
--- src/pages/app/anamnse/anamnse.tsx
+++ src/pages/app/anamnse/anamnse.tsx
@@ -1,49 +1,65 @@
-import { Helmet } from "react-helmet-async";
-import * as Tabs from '@radix-ui/react-tabs';
-import { VisaoGeral } from "./visao-geral";
-import { DadosAthetas } from "./dados-atheta";
-import { Fingerprint, LayoutDashboard, User } from "lucide-react";
-import { DadosPessoais } from "./dados-pessoais";
+import { Helmet } from 'react-helmet-async'
+import * as Tabs from '@radix-ui/react-tabs'
+import { VisaoGeral } from './visao-geral'
+import { DadosAthetas } from './dados-atheta'
+import { Fingerprint, LayoutDashboard, User, Stethoscope } from 'lucide-react'
+import { DadosPessoais } from './dados-pessoais'
+import { AlergiasEliminacoesFisiologicas } from './alergias-eliminacoes-fisiologicas'
 
-export function Anamnse () {
-    return (
-        <>
-            <Helmet title="Anamnse"/>
+export function Anamnse() {
+  return (
+    <>
+      <Helmet title="Anamnse" />
 
-                <div className="space-y-2 mb-3">
-                    <h1 className="text-3xl font-bold tracking-tight">Anamnse</h1>
-                        <p className="text-base text-slate-400">
-                            Visualize as informações da anamnse do atleta.
-                        </p>
-                </div> 
+      <div className="space-y-2 mb-3">
+        <h1 className="text-3xl font-bold tracking-tight">Anamnse</h1>
+        <p className="text-base text-slate-400">
+          Visualize as informações da anamnse do atleta.
+        </p>
+      </div>
 
-                <Tabs.Root defaultValue="tab1" className="flex items-start">
-                    <Tabs.List className="flex flex-col bg-slate-800 p-2 mr-6 w-80 rounded-md">
-                        <Tabs.Trigger value="tab1" className="flex items-center p-3">
-                            <div className="flex gap-4 items-center">
-                                <LayoutDashboard className="h-4 w-4"/>
-                                Visão Geral
-                            </div>
-                        </Tabs.Trigger>
-                        <Tabs.Trigger value="tab2" className="flex items-center p-3">
-                            <div className="flex gap-4 items-center">
-                                <User className="h-4 w-4"/>
-                                Dados Pessoais
-                            </div>
-                        </Tabs.Trigger>
-                        <Tabs.Trigger value="tab3" className="flex items-center p-3">
-                            <div className="flex gap-4 items-center">
-                                <Fingerprint className="h-4 w-4"/>
-                                Dados Atleta
-                            </div>
-                        </Tabs.Trigger>
-                    </Tabs.List>
-                    <div className="bg-slate-800 w-full flex flex-col rounded-md p-5">
-                    <Tabs.Content value="tab1"> <VisaoGeral /> </Tabs.Content>
-                    <Tabs.Content value="tab2"> <DadosPessoais /> </Tabs.Content>
-                    <Tabs.Content value="tab3"> <DadosAthetas /> </Tabs.Content>
-                    </div>
-                </Tabs.Root>
-        </>
-    )
+      <Tabs.Root defaultValue="tab1" className="flex items-start">
+        <Tabs.List className="flex flex-col bg-slate-800 p-2 mr-6 w-80 rounded-md">
+          <Tabs.Trigger value="tab1" className="flex items-center p-3">
+            <div className="flex gap-4 items-center">
+              <LayoutDashboard className="h-4 w-4" />
+              Visão Geral
+            </div>
+          </Tabs.Trigger>
+          <Tabs.Trigger value="tab2" className="flex items-center p-3">
+            <div className="flex gap-4 items-center">
+              <User className="h-4 w-4" />
+              Dados Pessoais
+            </div>
+          </Tabs.Trigger>
+          <Tabs.Trigger value="tab3" className="flex items-center p-3">
+            <div className="flex gap-4 items-center">
+              <Fingerprint className="h-4 w-4" />
+              Dados Atleta
+            </div>
+          </Tabs.Trigger>
+          <Tabs.Trigger value="tab4" className="flex items-center p-3">
+            <div className="flex gap-4 items-center">
+              <Stethoscope className="h-4 w-4" />
+              Sáude do atleta
+            </div>
+          </Tabs.Trigger>
+        </Tabs.List>
+        <div className="bg-slate-800 w-full flex flex-col rounded-md p-5">
+          <Tabs.Content value="tab1">
+            <VisaoGeral />
+          </Tabs.Content>
+          <Tabs.Content value="tab2">
+            <DadosPessoais />
+          </Tabs.Content>
+          <Tabs.Content value="tab3">
+            <DadosAthetas />
+          </Tabs.Content>
+          <Tabs.Content value="tab4">
+            <AlergiasEliminacoesFisiologicas />
+          </Tabs.Content>
+        </div>
+      </Tabs.Root>
+    </>
+  )
 }
